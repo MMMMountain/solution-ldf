ee77e7328b4861f435dcd6f7e3ecc72e0d5cab54
lidengfeng <623980474@qq.com>
2024-01-23 14:53:07 +0800

    feat:添加vue的自动引入

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const number = ref<number>(100)
+console.log(number, 99999)
+</script>
 
 <template>
   <main>
-    <a-button type="primary">Primary Button</a-button>
+    <a-button type="primary">Primary Button1</a-button>
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
@@ -17,6 +18,9 @@ export default defineConfig({
           importStyle: false // css in js
         })
       ]
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router']
     })
   ],
   resolve: {
